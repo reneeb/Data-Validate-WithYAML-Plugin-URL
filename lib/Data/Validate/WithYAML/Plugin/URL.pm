bd0ce4711f68049624db70e33ab9da1e76cd19be
reneeb <reneeb@dca9f4a1-6dba-46ad-9046-e9fb5d59a48b>
2011-11-15 10:05:26 +0000

    add URL plugin for Data::Validate::WithYAML
    git-svn-id: http://reneeb-svn.cvsdude.com/EigeneProjekte/cpan/Data-Validate-WithYAML-Plugin-URL@2105 dca9f4a1-6dba-46ad-9046-e9fb5d59a48b

---
+++ lib/Data/Validate/WithYAML/Plugin/URL.pm
@@ -0,0 +1,85 @@
+package Data::Validate::WithYAML::Plugin::URL;
+
+use warnings;
+use strict; 
+
+use Carp;
+
+use Regexp::Common qw(URI);
+
+=head1 NAME
+
+Data::Validate::WithYAML::Plugin::URL - Plugin to check URL
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use Data::Validate::WithYAML::Plugin::URL;
+
+    my $foo = Data::Validate::WithYAML::Plugin::URL->check( 'http://test.de/' );
+    ...
+    
+    # use the plugin via Data::Validate::WithYAML
+    
+    use Data::Validate::WithYAML;
+    
+    my $URL     = 'http://test.de/';
+    my $validator = Data::Validate::WithYAML->new( 'test.yml' );
+    print "yes" if $validator->check( 'URL', $URL );
+
+test.yml
+
+  ---
+  step1:
+      URL:
+          plugin: URL
+          type: required
+  
+
+=head1 SUBROUTINES
+
+=head2 check
+
+=cut
+
+sub check {
+    my ($class, $value) = @_;
+    
+    croak "no value to check" unless defined $value;
+    
+    my $return = 0;
+    if( $value =~ m{\A $RE{URI}{HTTP}{-scheme => qr/https?/} \z}x ){
+        $return = 1;
+    }
+    return $return;
+}
+
+=head1 AUTHOR
+
+Renee Baecker, C<< <module at renee-baecker.de> >>
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Data::Validate::WithYAML::Plugin::URL
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011 Renee Baecker, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;
